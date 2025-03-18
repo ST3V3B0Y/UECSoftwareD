330c7c1ca55da771357d92f9389f9ca24d51196f
steve <st3v3njaramillo@gmail.com>
2025-03-18 12:10:11 -0500

    QR y Base de Datos

--- app/models/facultad.py
+++ app/models/facultad.py
@@ -1,7 +1,7 @@
 from app import db
-
+ 
 class Facultad(db.Model):
     __tablename__='facultad'
     idFacultad = db.Column(db.Integer, primary_key=True)
     nombreFacultad = db.Column(db.String(256), nullable=False)
-    usuarios = db.relationship('Usuario', backref='facultad', lazy=True)
+    usuarios = db.relationship('Usuario', backref='facultad', lazy=True, cascade='all, delete, delete-orphan')
